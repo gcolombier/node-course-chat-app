3406efeff67c5af205577cccd31a8fdfbd89aba3
Guillaume Colombier <gcolombier@skyrepublic.com>
2018-09-12 07:19:00 -0700

    fixing port assignment issue

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-  console.log('server up on port 3000');
+app.listen(port, () => {
+  console.log('server up on port '+port);
 });
 
 
