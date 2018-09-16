3642d5a893dbaef8625376a96092803c254a520f
Guillaume Colombier <gcolombier@skyrepublic.com>
2018-09-16 11:48:02 -0700

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect',  function() {
+  console.log('connected to server');
+
+  socket.emit('createMessage', {
+    from:'Guillaume',
+    text:'Hello team'
+  });
+
+});
+
+socket.on('disconnect', function() {
+  console.log('disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('New Message', message);
+});
