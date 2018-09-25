a5e1f4f9e6d4ce1faf55a172ca82974e7beeba68
Guillaume Colombier <gcolombier@skyrepublic.com>
2018-09-24 20:47:49 -0700

    add css for chat page

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) => {
     console.log('createMessage', message)
 
     io.emit('newMessage',generateMessage(message.from, message.text));
-    callback('this is from the server');
+    callback();
 
   });
 
